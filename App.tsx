917e9281e7de6654e7058422117fa99e7c54a8e1
AmanDevelops <amanpal2072004@gmail.com>
2025-11-01 22:07:20 +0530

    Add initial implementation of Weight Tracker App with UI components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  Appbar,
+  Button,
+  PaperProvider,
+  Text,
+  TextInput,
+} from "react-native-paper";
 export default function App() {
+  const [text, setText] = useState("");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Appbar.Header>
+        <Appbar.Content title="Weight Tracker App" />
+      </Appbar.Header>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome to Weight Tracker App!</Text>
+
+        <TextInput
+          label="Enter Weight"
+          value={text}
+          onChangeText={(text) => setText(text)}
+          mode="outlined"
+          numberOfLines={1}
+          keyboardType="numeric"
+          style={styles.input}
+        />
+
+        <Button
+          icon="android"
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+          style={styles.button}
+        >
+          Add to Log
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    padding: 20,
+    paddingTop: 150,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontFamily: "Bitcount",
+  },
+  button: {
+    marginTop: 10,
+    width: "100%",
+  },
+  input: {
+    width: "100%",
   },
 });
